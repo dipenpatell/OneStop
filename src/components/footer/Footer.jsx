226f1fc76a20544254f2e05767b5e7ee46589ce3
Dipen Patel <dipenpatel0234@gmail.com>
2023-04-13 11:15:31 +0530

    sec comm

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
         <div className="footer__content__logo">
           <div className="logo">
             <img src={logo} alt="" />
-            <Link to="/">MoviesHunt</Link>
+            <Link to="/">OneStop</Link>
           </div>
         </div>
         <div className="footer__content__menus">
